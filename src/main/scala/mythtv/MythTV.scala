71f7a00131cca2a589bb49319996ad24691d1892
Tom Grigg <tomegrigg@gmail.com>
2016-09-27 17:13:14 -0700

    add some methods to MythTV object

--- src/main/scala/mythtv/MythTV.scala
+++ src/main/scala/mythtv/MythTV.scala
@@ -1,4 +1,11 @@
 package mythtv
 
+import model.Frontend
+import services.Service
+
 object MythTV {
+  def frontend(host: String): MythFrontend = new MythFrontend(host)
+  def backend(host: String): MythBackend = new MythBackend(host)
+  def service[A <: Service]: Service = ???
+  def discoverFrontends: List[Frontend] = ???
 }
