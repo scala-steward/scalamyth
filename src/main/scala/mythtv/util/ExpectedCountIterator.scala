9469bf2b38b288d67dc203c2e343f85c5561a99c
Tom Grigg <tomegrigg@gmail.com>
2016-10-04 12:27:18 -0700

    override default toString in ExpectedCountIterator

--- src/main/scala/mythtv/util/ExpectedCountIterator.scala
+++ src/main/scala/mythtv/util/ExpectedCountIterator.scala
@@ -3,8 +3,14 @@ package util
 
 import scala.collection.AbstractIterator
 
+// TODO potentitally add a 'remainingCount' state, which would also be included in toString()
+// TODO override hasDefiniteSize ? how about length ?
 class ExpectedCountIterator[A](val expectedCount: Int, underlying: Iterator[A])
     extends AbstractIterator[A] {
   override def hasNext: Boolean = underlying.hasNext
   override def next(): A = underlying.next()
+  override def toString: String = {
+    val items = if (expectedCount == 1) "item" else "items"
+    underlying.toString + s" of $expectedCount $items"
+  }
 }
