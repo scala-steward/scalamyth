b9a811188f82ec1e572a7a5062e41fd8acc2681a
Tom Grigg <tomegrigg@gmail.com>
2016-10-05 23:20:38 -0700

    notes on MythProtocol versions <-> MythTV release versions

--- src/main/scala/mythtv/connection/myth/MythProtocol.scala
+++ src/main/scala/mythtv/connection/myth/MythProtocol.scala
@@ -20,5 +20,8 @@ object MythProtocol extends MythProtocol {
   final val SPLIT_PATTERN: String = Pattern.quote(BACKEND_SEP)
 }
 
-private[myth] trait MythProtocol75 extends MythProtocol with MythProtocolLike75
-private[myth] trait MythProtocol77 extends MythProtocol with MythProtocolLike77
+//                                                          protocol version 63  // myth 0.24.x
+//                                                          protocol version 72  // myth 0.25.x
+private[myth] trait MythProtocol75 extends MythProtocol with MythProtocolLike75  // myth 0.26.x
+private[myth] trait MythProtocol77 extends MythProtocol with MythProtocolLike77  // myth 0.27.x
+//                                                          protocol version 88  // myth 0.28.x
