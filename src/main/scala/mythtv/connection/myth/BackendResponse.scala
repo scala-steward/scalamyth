8a250e155e1cd90506a2c2bf4066eb739eee113b
Tom Grigg <tomegrigg@gmail.com>
2016-10-11 21:06:15 -0700

    Refine BackendResponse trait/companion object
     - make split method concrete in BackendResponse trait
     - make companion object private

--- src/main/scala/mythtv/connection/myth/BackendResponse.scala
+++ src/main/scala/mythtv/connection/myth/BackendResponse.scala
@@ -4,13 +4,11 @@ package myth
 
 trait BackendResponse extends Any {
   def raw: String
-  def split: Array[String]
+  def split: Array[String] = raw split MythProtocol.SPLIT_PATTERN
 }
 
-object BackendResponse {
+private object BackendResponse {
   def apply(r: String): BackendResponse = Response(r)
 }
 
-private final case class Response(raw: String) extends AnyVal with BackendResponse {
-  def split: Array[String] = raw split MythProtocol.SPLIT_PATTERN
-}
+private final case class Response(raw: String) extends AnyVal with BackendResponse
