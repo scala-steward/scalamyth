5aa350322f9289b5dc983c56023b5c144d6fd90d
Tom Grigg <tomegrigg@gmail.com>
2016-10-12 13:22:21 -0700

    new class EventLock

--- src/main/scala/mythtv/connection/myth/EventConnection.scala
+++ src/main/scala/mythtv/connection/myth/EventConnection.scala
@@ -4,6 +4,8 @@ package myth
 
 import java.net.InetAddress
 
+import scala.concurrent.duration.Duration
+
 import EnumTypes.MythProtocolEventMode
 
 trait BackendEvent extends Any with BackendResponse {
@@ -40,8 +42,31 @@ trait EventConnection extends SocketConnection { /* with EventProtocol ?? */
   def addListener(listener: EventListener): Unit
   def removeListener(listener: EventListener): Unit
 
-  def += (listener: EventListener): Unit = addListener(listener)
-  def -= (listener: EventListener): Unit = removeListener(listener)
+  final def += (listener: EventListener): Unit = addListener(listener)
+  final def -= (listener: EventListener): Unit = removeListener(listener)
+}
+
+final class EventLock(eventConn: EventConnection, eventFilter: (BackendEvent) => Boolean)
+    extends EventListener {
+  private[this] var locked = true
+
+  eventConn.addListener(this)
+
+  def listenFor(event: BackendEvent): Boolean = eventFilter(event)
+
+  def handle(event: BackendEvent): Unit = {
+    synchronized {
+      locked = false
+      notifyAll()
+    }
+    eventConn.removeListener(this)
+  }
+
+  // TODO timeout doesn't do what you might think, and is of questionable use here
+  def waitFor(timeout: Duration = Duration.Inf): Unit = {
+    val millis = if (timeout.isFinite()) timeout.toMillis else 0
+    synchronized { while (locked) wait(millis) }
+  }
 }
 
 private abstract class AbstractEventConnection(host: String, port: Int, timeout: Int,
