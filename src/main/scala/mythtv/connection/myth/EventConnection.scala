3c5bf1b2323ec32dd58b331d23e7571d60757a12
Tom Grigg <tomegrigg@gmail.com>
2016-10-15 20:49:35 -0700

    Moved EventLock to its own file

--- src/main/scala/mythtv/connection/myth/EventConnection.scala
+++ src/main/scala/mythtv/connection/myth/EventConnection.scala
@@ -4,8 +4,6 @@ package myth
 
 import java.net.{ InetAddress, SocketException }
 
-import scala.concurrent.duration.Duration
-
 import EnumTypes.MythProtocolEventMode
 
 trait BackendEvent extends Any with BackendResponse {
@@ -46,54 +44,6 @@ trait EventConnection extends SocketConnection { /* with EventProtocol ?? */
 
   final def += (listener: EventListener): Unit = addListener(listener)
   final def -= (listener: EventListener): Unit = removeListener(listener)
-}
-
-trait EventLock {
-  def event: Option[BackendEvent]
-  // TODO timeout doesn't do what you might think, and is of questionable use here
-  def waitFor(timeout: Duration = Duration.Inf): Unit
-}
-
-object EventLock {
-  def apply(eventConn: EventConnection, eventFilter: (BackendEvent) => Boolean): EventLock =
-    new Lock(eventConn, eventFilter)
-
-  /**
-    * An event lock without an event; waitFor always returns immediately and
-    * event is always None.
-    */
-  def empty: EventLock = Empty
-
-  private final class Lock(eventConn: EventConnection, eventFilter: (BackendEvent) => Boolean)
-      extends EventListener with EventLock {
-    private[this] var locked = true
-    @volatile private[this] var unlockEvent: Option[BackendEvent] = None
-
-    eventConn.addListener(this)
-
-    def listenFor(event: BackendEvent): Boolean = eventFilter(event)
-
-    def handle(event: BackendEvent): Unit = {
-      synchronized {
-        locked = false
-        unlockEvent = Some(event)
-        notifyAll()
-      }
-      eventConn.removeListener(this)
-    }
-
-    def event: Option[BackendEvent] = unlockEvent
-
-    def waitFor(timeout: Duration = Duration.Inf): Unit = {
-      val millis = if (timeout.isFinite()) timeout.toMillis else 0
-      synchronized { while (locked) wait(millis) }
-    }
-  }
-
-  private object Empty extends EventLock {
-    def event: Option[BackendEvent] = None
-    def waitFor(timeout: Duration = Duration.Inf): Unit = ()
-  }
 }
 
 // TODO don't use infinite timeout for protocol negotiation
