5f8294b7bdbc2353781d24ffb058de609fefa598
Tom Grigg <tomegrigg@gmail.com>
2016-10-17 18:59:01 -0700

    added some component accessors and factory methods to MythDateTime

--- src/main/scala/mythtv/util/MythDateTime.scala
+++ src/main/scala/mythtv/util/MythDateTime.scala
@@ -9,17 +9,29 @@ import java.time.format.DateTimeFormatter
 class MythDateTime(val instant: Instant) extends AnyVal {
   private def offsetDt = instant atOffset ZoneOffset.UTC
 
+  // accessors
+  def year: Int = offsetDt.getYear
+  def month: Int = offsetDt.getMonthValue
+  def day: Int = offsetDt.getDayOfMonth
+
+  def hour: Int = offsetDt.getHour
+  def minute: Int = offsetDt.getMinute
+  def second: Int = offsetDt.getSecond
+
+  // formatters
   import MythDateTime.FORMATTER_MYTH
   def mythformat: String = offsetDt format FORMATTER_MYTH
   def toMythFormat: String = mythformat
   def toIsoFormat: String = instant.toString
   def toNaiveIsoFormat: String = offsetDt format DateTimeFormatter.ISO_LOCAL_DATE_TIME
+  override def toString: String = instant.toString
+
+  // converters
   def toTimestamp: Long = instant.getEpochSecond
   def toInstant: Instant = instant
   def toLocalDateTime: LocalDateTime = offsetDt.toLocalDateTime   // is this naive UTC? TODO rename method?
   def toOffsetDateTime: OffsetDateTime = offsetDt
   def toZonedDateTime: ZonedDateTime = instant atZone ZoneOffset.UTC
-  override def toString: String = instant.toString
 }
 
 object MythDateTime {
@@ -33,12 +45,18 @@ object MythDateTime {
   implicit def javaInstant2MythDt(instant: Instant) = new MythDateTime(instant)
   implicit def javaLocalDt2MythDt(dt: LocalDateTime) = new MythDateTime(dt)
    */
+  def apply(instant: Instant) = new MythDateTime(instant)
+
+  def apply(year: Int, month: Int, day: Int, hour: Int, minute: Int, second: Int) =
+    new MythDateTime(
+      OffsetDateTime.of(year, month, day, hour, minute, second, 0, ZoneOffset.UTC).toInstant)
+
   private def apply(localDt: LocalDateTime) = new MythDateTime(localDt.toInstant(ZoneOffset.UTC))
 
   def fromMythFormat(mdt: String): MythDateTime = MythDateTime(LocalDateTime.parse(mdt, FORMATTER_MYTH))
 
   // this is strict ISO format with UTC ("Z") timezone
-  def fromIso(isoDt: String): MythDateTime = new MythDateTime(Instant.parse(isoDt))
+  def fromIso(isoDt: String): MythDateTime = MythDateTime(Instant.parse(isoDt))
 
   // this is naive ISO format (no timezone, implied to be UTC)
   def fromNaiveIso(isoDt: String): MythDateTime = MythDateTime(LocalDateTime.parse(isoDt))
@@ -48,9 +66,9 @@ object MythDateTime {
   def fromNaiveIsoLoose(isoDt: String): MythDateTime =
     MythDateTime(LocalDateTime.parse(isoDt, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm[:ss]")))
 
-  def fromTimestamp(ts: Long): MythDateTime = new MythDateTime(Instant.ofEpochSecond(ts))
+  def fromTimestamp(ts: Long): MythDateTime = MythDateTime(Instant.ofEpochSecond(ts))
 
-  def now: MythDateTime = new MythDateTime(Instant.now)
+  def now: MythDateTime = MythDateTime(Instant.now)
 
   object MythDateTimeOrdering extends Ordering[MythDateTime] {
     def compare(x: MythDateTime, y: MythDateTime) = x.instant compareTo y.instant
