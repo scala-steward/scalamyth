e018c783b089a138000b120d08f01aad5d00170a
Tom Grigg <tomegrigg@gmail.com>
2016-10-27 17:28:16 -0700

    make GenericBackend* objects private to package 'myth'

--- src/main/scala/mythtv/connection/myth/data/GenericBackendObject.scala
+++ src/main/scala/mythtv/connection/myth/data/GenericBackendObject.scala
@@ -3,14 +3,14 @@ package connection
 package myth
 package data
 
-abstract class GenericBackendObject(data: Seq[String], fieldOrder: IndexedSeq[String]) {
+private[myth] abstract class GenericBackendObject(data: Seq[String], fieldOrder: IndexedSeq[String]) {
   // assumes data.length >= fieldOrder.length, or else some fields will be missing
   val fields: Map[String, String] = (fieldOrder zip data).toMap
   def apply(fieldName: String): String = fields(fieldName)
   def get(fieldName: String): Option[String] = fields.get(fieldName)
 }
 
-trait GenericBackendObjectFactory[+A] {
+private[myth] trait GenericBackendObjectFactory[+A] {
   def FIELD_ORDER: IndexedSeq[String]
   def apply(data: Seq[String]): A
 }
