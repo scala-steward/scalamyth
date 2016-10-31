61d7895c85d67d1cd32a4c36f3ca0da6cacd85c1
Tom Grigg <tomegrigg@gmail.com>
2016-10-30 21:30:29 -0700

    fix up ServiceProvider interface to be usable

--- src/main/scala/mythtv/MythTV.scala
+++ src/main/scala/mythtv/MythTV.scala
@@ -1,11 +1,11 @@
 package mythtv
 
 import model.Frontend
-import services.Service
+import services._
 
 object MythTV {
   def frontend(host: String): MythFrontend = new MythFrontend(host)
   def backend(host: String): MythBackend = new MythBackend(host)
-  def service[A <: Service]: A = ???
+  def service[A <: Service](host: String)(implicit provider: ServiceProvider[A]): A = provider.instance(host)
   def discoverFrontends: List[Frontend] = ???
 }
--- src/main/scala/mythtv/services/ServiceProvider.scala
+++ src/main/scala/mythtv/services/ServiceProvider.scala
@@ -2,5 +2,5 @@ package mythtv
 package services
 
 trait ServiceProvider[A] {
-  def instance[B <: A]: B  // TODO this will not work well if I extend multiple service providers in a class!?
+  def instance(host: String): A
 }
