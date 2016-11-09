5b4e50c9124ba160ba7440e2fd9e6b6cd2bae36b
Tom Grigg <tomegrigg@gmail.com>
2016-11-08 20:57:32 -0800

    First draft (very rough!) of Myth protocol file transfer support

--- src/main/scala/mythtv/connection/myth/FileTransfer.scala
+++ src/main/scala/mythtv/connection/myth/FileTransfer.scala
@@ -3,3 +3,191 @@ package connection
 package myth
 
 final case class FileTransferId(id: Int) extends AnyVal
+
+/**
+  * The API in use over the control channel during Myth protocol file transfers
+  * using the QUERY_FILETRANSFER series of protocol commands.
+  */
+trait MythFileTransferAPI {
+  def done(): Unit
+  def isOpen: Boolean
+  def reopen(newFileName: String): Boolean
+  def requestBlock(blockSize: Int): Int
+  def requestSize: Long
+  def seek(pos: Long, whence: Int, currentPos: Long): Long
+  def setTimeout(fast: Boolean): Unit
+  def writeBlock(blockSize: Int): Int
+}
+
+class MythFileTransferObject(val ftId: FileTransferId, conn: MythProtocolAPI) extends MythFileTransferAPI {
+  def done(): Unit = conn.queryFileTransferDone(ftId)
+  def isOpen: Boolean = conn.queryFileTransferIsOpen(ftId)
+  def reopen(newFileName: String): Boolean = conn.queryFileTransferReopen(ftId, newFileName)
+  def requestBlock(blockSize: Int): Int = conn.queryFileTransferRequestBlock(ftId, blockSize)
+  def requestSize: Long = conn.queryFileTransferRequestSize(ftId)
+  def seek(pos: Long, whence: Int, currentPos: Long): Long = conn.queryFileTransferSeek(ftId, pos, whence, currentPos)
+  def setTimeout(fast: Boolean): Unit = conn.queryFileTransferSetTimeout(ftId, fast)
+  def writeBlock(blockSize: Int): Int = conn.queryFileTransferWriteBlock(ftId, blockSize)
+}
+
+object MythFileTransferObject {
+  def apply(controlChannel: BackendAPIConnection, dataChannel: FileTransferConnection) =
+    new MythFileTransferObject(dataChannel.transferId, controlChannel)
+}
+
+// TODO what happens if we specify a file that does not exist?
+object FileTransfer {  // TODO this doesn't specify read/write mode
+  def apply(host: String, fileName: String, storageGroup: String): FileTransfer = {
+    // TODO how will control channel get closed since it's embeedded here and FT doesn't know that it owns it...
+    val controlChannel = BackendAPIConnection(host)
+    apply(controlChannel, fileName, storageGroup)
+  }
+
+  def apply(controlChannel: BackendAPIConnection, fileName: String, storageGroup: String): FileTransfer = {
+    val dataChannel = FileTransferConnection(controlChannel.host, fileName, storageGroup, controlChannel.port)
+    val fto = MythFileTransferObject(controlChannel, dataChannel)
+    new FileTransfer(fto, dataChannel)
+  }
+}
+
+// TODO are we opening data channel with event mode set (or something??)  Actually, it's readahead...
+//   Sending command ANN FileTransfer dove 0 1 2000[]:[]Music/Performances/Rolling_in_the_Deep.mp4[]:[]Videos
+
+class FileTransfer private[myth](controlChannel: MythFileTransferAPI, dataChannel: FileTransferConnection) {
+  // A file transfer requires two (optionally three) channels:
+  //   - control channel  (BackendConnection or BackendAPIConnection)
+  //   - data channel     (FileTransferConnection)
+  // and optionally
+  //   - event channel    (EventConnection)
+
+  // TODO support Java InputStream/OutputStream interfaces?
+
+  @volatile protected[myth] var size: Long = dataChannel.fileSize
+  protected[myth] var position: Long = 0L
+
+  private def clamp(value: Long, min: Long, max: Long): Long =
+    if (value < min) min
+    else if (value > max) max
+    else value
+
+  // close the file
+  def close(): Unit = {
+    controlChannel.done()
+    // TODO close the data channel (how? just shut down the socket?)
+  }
+
+  // current offset in file
+  def tell: Long = position
+
+  // seek to offset (relative to whence)
+  def seek(offset: Long, whence: Int): Unit = { // TODO enumeration for whence?
+    val adjOffset = whence match {
+      case 0 => clamp(offset, 0L, size)
+      case 1 => clamp(offset, -position, size - position)
+      case 2 => clamp(offset, -size, 0L) + size
+    }
+    val adjWhence = if (whence == 2) 0 else whence
+    val newPos: Long = controlChannel.seek(adjOffset, adjWhence, position)
+    if (newPos < 0) throw new RuntimeException("failed seek")
+    position = newPos
+  }
+
+  // seek to beginning
+  def rewind(): Unit = seek(0, whence=0)
+
+  // NB It seems that the myth backend won't send any blocks bigger than 128k no
+  //    matter what size we ask for. Is this a hard limit in the server code?
+
+  def read(buf: Array[Byte], off: Int, len: Int): Int = {
+    if (!dataChannel.isReadble) throw new RuntimeException("data channel is not readable")
+    val adjLen = clamp(len, 0, math.min(size - position, Int.MaxValue)).toInt
+
+    // TODO need to loop until we've read size bytes??  what are normal semantics of Java I/O?
+    var count = adjLen  // TODO what value is this really supposed to have? (leftover amount we need in loop)
+
+    val rc = controlChannel.requestBlock(count)
+    if (rc != count) {
+      if (rc < 0) {
+        // TODO failure; re-seek to current position and retry?
+        ???
+      }
+      else count = rc
+    }
+
+    val n = dataChannel.read(buf, off, count)
+    println(s"Read $n bytes from data channel (desired $count)")
+    position += n    // can 'n' and 'count' differ here? Yep!
+    n
+  }
+
+  def write(buf: Array[Byte], off: Int, len: Int): Unit = {
+    if (!dataChannel.isWriteable) throw new RuntimeException("data channel is not writeable")
+    if (len > 0) {
+      dataChannel.write(buf, off, len)
+      controlChannel.writeBlock(len)  // TODO utilize result value? or is it just parroted back to us?
+      position = math.max(position + len, size)
+    }
+  }
+}
+
+abstract class EventingFileTransfer(
+  controlChannel: MythFileTransferAPI,
+  dataChannel: FileTransferConnection,
+  eventChannel: EventConnection
+) extends FileTransfer(controlChannel, dataChannel) {
+
+  eventChannel.addListener(listener)
+
+  protected def listener: EventListener
+
+  override def close(): Unit = {
+    super.close()
+    eventChannel.removeListener(listener)
+  }
+}
+
+class RecordingFileTransfer(
+  controlChannel: MythFileTransferAPI,
+  dataChannel: FileTransferConnection,
+  eventChannel: EventConnection
+) extends EventingFileTransfer(controlChannel, dataChannel, eventChannel) {
+
+  override def listener: EventListener = updateListener
+
+  private[this] lazy val updateListener = new EventListener {
+    override def listenFor(event: BackendEvent): Boolean = event.isEventName("UPDATE_FILE_SIZE")
+
+    override def handle(event: BackendEvent): Unit = event.parse match {
+      case Event.UpdateFileSizeEvent(chanId, recStartTs, newSize) =>
+        // TODO ensure that chanId/recStartTs matches the recording we're transferring
+        size = newSize
+      case _ => ()
+    }
+  }
+}
+
+// TODO what exactly is this class for?
+// Reading/writing(?) from a file that is being downloaded to the server using DOWNLOAD_FILE ?
+class DownloadFileTransfer(
+  controlChannel: MythFileTransferAPI,
+  dataChannel: FileTransferConnection,
+  eventChannel: EventConnection
+) extends EventingFileTransfer(controlChannel, dataChannel, eventChannel) {
+
+  override protected def listener: EventListener = downloadListener
+
+  private[this] lazy val downloadListener = new EventListener {
+    override def listenFor(event: BackendEvent): Boolean = event.isEventName("DOWNLOAD_FILE")
+
+    override def handle(event: BackendEvent): Unit = event.parse match {
+      case Event.DownloadFileUpdateEvent(url, fileName, received, total) =>
+        // TODO verify that the url/filename matches the file we're downloading
+        size = received  // TODO is this the update we want?
+      case Event.DownloadFileFinished(url, fileName, fileSize, err, errCode) =>
+        // TODO verify that the url/filename matches the file we're downloading
+        size = fileSize
+        // TODO initiate finalization of this object/mark as completed?
+      case _ => ()
+    }
+  }
+}
--- src/main/scala/mythtv/connection/myth/FileTransferConnection.scala
+++ src/main/scala/mythtv/connection/myth/FileTransferConnection.scala
@@ -4,19 +4,32 @@ package myth
 
 import util.NetworkUtil
 
-trait FileTransferConnection extends SocketConnection
+trait FileTransferConnection extends SocketConnection {
+  def transferId: FileTransferId
+  def fileSize: Long
 
-// TODO are we expected to send "DONE" command when finished with a FileTransfer socket?
-// TODO do I want a base class without the [String] reader/writer so I can read/write raw byte blocks?
+  def isReadble: Boolean = true    // FIXME
+  def isWriteable: Boolean = false // FIXME
+
+  def read(buf: Array[Byte], off: Int, len: Int): Int
+  def write(buf: Array[Byte], off: Int, len: Int): Unit
+}
+
+// TODO are we expected to send "DONE" command when finished with a FileTransfer socket? Python bindings don't seem to.
 private abstract class AbstractFileTransferConnection(host: String, port: Int, timeout: Int, fileName: String, storageGroup: String)
-    extends AbstractBackendConnection(host, port, timeout)
-    with FileTransferConnection {
+  extends AbstractBackendConnection(host, port, timeout)
+     with FileTransferConnection {
 
   self: MythProtocolAPI with AnnouncingConnection =>
 
-  protected def announce(): Unit = {
+  private[this] var ftId: FileTransferId = FileTransferId(0)
+  private[this] var filesize: Long = 0L
+
+  /*protected*/ def announce(): Unit = {
     val localHost = NetworkUtil.myHostName
     val (ftID, size) = announceFileTransfer(localHost, fileName, storageGroup)
+    this.ftId = ftID
+    this.filesize = size.bytes
   }
 
   override def sendCommand(command: String, args: Any*): Option[_] = {
@@ -24,4 +37,68 @@ private abstract class AbstractFileTransferConnection(host: String, port: Int, t
     else super.sendCommand(command, args: _*)
   }
 
+  override def transferId: FileTransferId = ftId
+
+  override def fileSize: Long = filesize
+
+  override def read(buf: Array[Byte], off: Int, len: Int): Int =
+    inputStream.read(buf, off, len)
+
+  override def write(buf: Array[Byte], off: Int, len: Int): Unit =
+    outputStream.write(buf, off, len)
+}
+
+private sealed trait FileTransferConnectionFactory {
+  def apply(host: String, port: Int, timeout: Int, fileName: String, storageGroup: String): FileTransferConnection
+}
+
+object FileTransferConnection {
+  private val supportedVersions = Map[Int, FileTransferConnectionFactory](
+    75 -> FileTransferConnection75,
+    77 -> FileTransferConnection77
+  )
+
+  def apply(
+    host: String,
+    fileName: String,
+    storageGroup: String,
+    port: Int = BackendConnection.DEFAULT_PORT,
+    timeout: Int = BackendConnection.DEFAULT_TIMEOUT
+  ): FileTransferConnection = {
+    try {
+      val factory = supportedVersions(BackendConnection.DEFAULT_VERSION)
+      factory(host, port, timeout, fileName, storageGroup)
+    } catch {
+      case ex @ WrongMythProtocolException(requiredVersion) =>
+        if (supportedVersions contains requiredVersion) {
+          val factory = supportedVersions(requiredVersion)
+          factory(host, port, timeout, fileName, storageGroup)
+        }
+        else throw new UnsupportedMythProtocolException(ex)
+    }
+  }
+}
+
+private class FileTransferConnection75(host: String, port: Int, timeout: Int, fileName: String, storageGroup: String)
+  extends AbstractFileTransferConnection(host, port, timeout, fileName, storageGroup)
+     with MythProtocol75
+     with MythProtocolAPI
+     with BackendAPILike
+     with AnnouncingConnection
+
+private object FileTransferConnection75 extends FileTransferConnectionFactory {
+  def apply(host: String, port: Int, timeout: Int, fileName: String, storageGroup: String): FileTransferConnection =
+    new FileTransferConnection75(host, port, timeout, fileName, storageGroup)
+}
+
+private class FileTransferConnection77(host: String, port: Int, timeout: Int, fileName: String, storageGroup: String)
+  extends AbstractFileTransferConnection(host, port, timeout, fileName, storageGroup)
+    with MythProtocol77
+    with MythProtocolAPI
+    with BackendAPILike
+    with AnnouncingConnection
+
+private object FileTransferConnection77 extends FileTransferConnectionFactory {
+  def apply(host: String, port: Int, timeout: Int, fileName: String, storageGroup: String): FileTransferConnection =
+    new FileTransferConnection77(host, port, timeout, fileName, storageGroup)
 }
