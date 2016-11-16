dea9e370a315b3b3642c6533aa0c95cc99d24bc4
Tom Grigg <tomegrigg@gmail.com>
2016-11-16 12:48:46 -0800

    added VideoPosition trait and VideoPositionSeconds class

--- src/main/scala/mythtv/model/data.scala
+++ src/main/scala/mythtv/model/data.scala
@@ -11,17 +11,39 @@ trait Frontend extends FrontendOperations
 /* We define the Ordering objects in companion objects to the case classes
    because nested objects are currently prohibited inside value classes. */
 
+sealed trait VideoPosition extends Any {
+  def pos: Long
+  def units: String
+}
+
 /**
  * Represents the position of a video stream as a frame number.
  */
-final case class VideoPositionFrame(pos: Long) extends AnyVal
-
-object VideoPositionFrame {
-  object VideoPositionFrameOrdering extends Ordering[VideoPositionFrame] {
-    def compare(x: VideoPositionFrame, y: VideoPositionFrame): Int = x.pos compare y.pos
-  }
-  implicit def ordering: Ordering[VideoPositionFrame] = VideoPositionFrameOrdering
+final case class VideoPositionFrame(pos: Long) extends AnyVal with VideoPosition {
+  def units = "f"
 }
+
+/**
+  * Represents the position of a video stream as number of seconds from the beginning.
+  */
+final case class VideoPositionSeconds(pos: Long) extends AnyVal with VideoPosition {
+  def units = "s"
+}
+
+private[model] trait GenericVideoPositionCompanion[T <: VideoPosition] {
+  /* This ordering object is only intended to be used on like subclasses of VideoPosition,
+     e.g. comparing a VideoPositionFrame with a VideoPositionFrame, or a VideoPositionSeconds
+     with a VideoPositionSeconds. If used on a heterogeous sequence of VideoPosition, the
+     results may not be as expected. */
+  object VideoPositionOrdering extends Ordering[T] {
+    def compare(x: T, y: T): Int = x.pos compare y.pos
+  }
+  implicit def ordering: Ordering[T] = VideoPositionOrdering
+}
+
+object VideoPosition extends GenericVideoPositionCompanion[VideoPosition]
+object VideoPositionFrame extends GenericVideoPositionCompanion[VideoPositionFrame]
+object VideoPositionSeconds extends GenericVideoPositionCompanion[VideoPositionSeconds]
 
 trait VideoSegment {
   def start: VideoPositionFrame
