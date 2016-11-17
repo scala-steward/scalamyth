22a230907873f13d2b3ac1e63ee9501992b80947
Tom Grigg <tomegrigg@gmail.com>
2016-11-17 13:53:49 -0800

    new event monitor example program

---
+++ src/main/scala/examples/eventmon/EventMonitor.scala
@@ -0,0 +1,30 @@
+package examples.eventmon
+
+import java.time.LocalDateTime
+
+import mythtv._
+import model._
+import util._
+import connection.myth._
+
+class Listener extends EventListener {
+  def listenFor(event: Event): Boolean = true
+  def handle(event: Event): Unit = {
+    log(event)
+  }
+
+  private def log(ev: Event): Unit = {
+    println(LocalDateTime.now.toString + " " + ev.toString)
+  }
+}
+
+class EventMonitor(host: String) {
+  val conn = EventConnection(host, eventMode = MythProtocolEventMode.Normal)
+  conn += new Listener
+}
+
+object EventMonitor {
+  def main(args: Array[String]): Unit = {
+    new EventMonitor("myth1")
+  }
+}
