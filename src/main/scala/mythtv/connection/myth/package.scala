8581fe8b2a01e23be79e9cf5423e368b14d702ec
Tom Grigg <tomegrigg@gmail.com>
2016-11-19 19:46:29 -0800

    new (experimental) implicit class MythProtocolResultGetter; needs refinement

--- src/main/scala/mythtv/connection/myth/package.scala
+++ src/main/scala/mythtv/connection/myth/package.scala
@@ -13,4 +13,18 @@ package object myth {
   implicit class CharsetStringEncoder(val enc: CharsetEncoder) extends AnyVal {
     def encode(str: String): ByteBuffer = enc.encode(CharBuffer.wrap(str))
   }
+
+  implicit class MythProtocolResultGetter[T](val res: MythProtocolResult[T]) extends AnyVal {
+    import MythProtocolFailure._
+
+    def get: T = res match {
+      case Right(result) => result
+      case Left(fail) => fail match {
+        case MythProtocolNoResult => throw new RuntimeException("no result")              // TODO handle better
+        case MythProtocolFailureUnknown => throw new RuntimeException("unknown failure")  // TODO handle better
+        case MythProtocolFailureMessage(msg) => throw new RuntimeException(msg)           // TODO handle better
+        case MythProtocolFailureThrowable(ex) => throw ex
+      }
+    }
+  }
 }
