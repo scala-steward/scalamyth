dc01ed2d19a0051b9c5b72b9612b1081eeae3099
Tom Grigg <tomegrigg@gmail.com>
2016-11-23 12:00:27 -0800

    fix typo

--- src/main/scala/mythtv/connection/myth/FileTransferChannel.scala
+++ src/main/scala/mythtv/connection/myth/FileTransferChannel.scala
@@ -111,7 +111,7 @@ private[myth] class FileTransferChannelImpl(controlChannel: FileTransferAPI, dat
       if (allotedSize < 0) {
         // TODO failure; re-seek to current position and retry (a maximum number of times?)
       } else if (allotedSize == 0) {
-        canReadMore = false   // TODO is this the righ thing to do here?
+        canReadMore = false   // TODO is this the right thing to do here?
       } else {
         var bytesReadThisRequest: Int = 0
 
