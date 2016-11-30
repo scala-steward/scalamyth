b4e50073988fc7d73cb6a068ec648b58a57cc353
Tom Grigg <tomegrigg@gmail.com>
2016-11-30 09:40:50 -0800

    Adjust URIFactory to fix broken URIs containing whitespace

--- src/main/scala/mythtv/util/URIFactory.scala
+++ src/main/scala/mythtv/util/URIFactory.scala
@@ -1,12 +1,22 @@
 package mythtv
 package util
 
-import java.net.URI
+import java.net.{ URI, URISyntaxException }
 
 private[mythtv] object URIFactory {
   private val emptyUri = new URI("")
 
   def empty: URI = emptyUri
 
-  def apply(s: String): URI = if (s.isEmpty) empty else new URI(s)
+  // FIXME UPSTREAM VideoService (all Services API?) does not properly escape URLs for artwork
+
+  def apply(s: String): URI = {
+    if (s.isEmpty) empty
+    else {
+      try new URI(s)
+      catch {  // try a simple fix of whitespace escaping on syntax error
+        case _: URISyntaxException => new URI(s.replace(" ", "%20"))
+      }
+    }
+  }
 }
