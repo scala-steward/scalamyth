d7c9a6acad8077cb8149a08448165f4e129b348c
Tom Grigg <tomegrigg@gmail.com>
2016-11-30 10:35:49 -0800

    Added ServiceResultGetter implicit class

--- src/main/scala/mythtv/services/package.scala
+++ src/main/scala/mythtv/services/package.scala
@@ -4,4 +4,17 @@ import services.Service.ServiceFailure
 
 package object services {
   type ServiceResult[T] = Either[ServiceFailure, T]
+
+  implicit class ServiceResultGetter[T](val res: ServiceResult[T]) extends AnyVal {
+    import ServiceFailure._
+
+    def get: T = res match {
+      case Right(result) => result
+      case Left(fail) => fail match {
+        case ServiceNoResult => throw new NoSuchElementException                    // TODO handle better
+        case ServiceFailureUnknown => throw new RuntimeException("unknown failure") // TODO handle better
+        case ServiceFailureThrowable(ex) => throw ex
+      }
+    }
+  }
 }
