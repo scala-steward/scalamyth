74e35b4325ff685f44e2a1eb2bff24f8e4ae7467
Tom Grigg <tomegrigg@gmail.com>
2016-12-17 08:19:19 -0800

    implement map and filter on PagedList so they return another PagedList
    otherwise they will implement Iterable which is less desirable

--- src/main/scala/mythtv/services/PagedList.scala
+++ src/main/scala/mythtv/services/PagedList.scala
@@ -9,4 +9,34 @@ trait PagedList[+A] extends Iterable[A] {
 
   def iterator: Iterator[A] = items.iterator
   override def toString: String = s"<PagedList $count/$totalAvailable @ $startIndex>"
+
+  // Override map, filter, filterNot so they return PagedList[_] rather than Iterable[_]
+
+  def map[B](f: (A) => B): PagedList[B] = {
+    val newItems = items map f
+    val (n, avail, index) = (count, totalAvailable, startIndex)
+    new PagedList[B] {   // avoid closing over old instance
+      def items          = newItems
+      def count          = n
+      def totalAvailable = avail
+      def startIndex     = index
+    }
+  }
+
+  // Note that filter does not affect the 'count' and 'totalAvailable' fields
+
+  private def plFilter(p: (A) => Boolean, isNegated: Boolean): PagedList[A] = {
+    val newItems = if (isNegated) items filterNot p else items filter p
+    val (n, avail, index) = (count, totalAvailable, startIndex)
+    new PagedList[A] {   // avoid closing over old instance
+      def items          = newItems
+      def count          = n
+      def totalAvailable = avail
+      def startIndex     = index
+    }
+  }
+
+  override def filter(p: (A) => Boolean): PagedList[A] = plFilter(p, isNegated = false)
+
+  override def filterNot(p: (A) => Boolean): PagedList[A] = plFilter(p, isNegated = true)
 }
