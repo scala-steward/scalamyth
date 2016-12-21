3f3ff58f18e43342dd9bb4c5346f3f77228859d3
Tom Grigg <tomegrigg@gmail.com>
2016-12-21 09:32:55 -0800

    added RecRuleFilterProvider (using services API only for now)

---
+++ src/main/scala/mythtv/RecRuleFilterProvider.scala
@@ -0,0 +1,14 @@
+package mythtv
+package util
+
+import services._
+
+class RecRuleFilterProvider(host: String) extends (() => Map[Int, String]) {
+  def apply(): Map[Int, String] = {
+    // This implementation will only work on 0.28+, when the GetRecRuleFilterList endpoint was added.
+    // For prior versions, we must read form the database directly.
+    val dvr = ServiceProvider.dvrService(host)
+    val filterList = dvr.getRecRuleFilterList.get
+    filterList.map(f => (1 << f.id, f.name))(collection.breakOut)
+  }
+}
