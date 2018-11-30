46a88c5e6f20ca45451f9a3fccbb3bcc64ce12d5
Tom Grigg <tomegrigg@gmail.com>
2018-11-30 12:41:28 -0800

    Examples: overhaul of RecXfer sample program
     - Command line argument parsing (utilize SampleProgram trait)
    
     - Add support for specifying a recording source
        (in-progress recordings or previously recorded)
    
     - File transfer code refactored out to Xfer trait

--- examples/src/main/scala/examples/protocol/RecXfer.scala
+++ examples/src/main/scala/examples/protocol/RecXfer.scala
@@ -1,42 +1,107 @@
 package examples.protocol
 
-import mythtv.connection.myth.{ MythProtocolAPIConnection, RecordingTransferChannel }
-import mythtv.model.{ CaptureCardId, Recording }
+import mythtv.connection.myth._
+import mythtv.model.{ CaptureCardId, ChanId, RecordedId, Recording }
+import mythtv.util.{ ByteCount, MythDateTime }
 
-object RecXfer {
-  def doTransfer(api: MythProtocolAPIConnection, rec: Recording): Long = {
+import MythProtocol.MythProtocolFailure
+import RecordedId.{ RecordedIdChanTime, RecordedIdInt }
+
+object RecXfer extends SampleProgram with Xfer {
+
+  def doTransfer(api: MythProtocolAPIConnection, rec: Recording): ByteCount = {
     val ft = RecordingTransferChannel(api, rec)
-    val buf = java.nio.ByteBuffer.allocate(256*1024)
-    val counts: collection.mutable.Set[Int] = collection.mutable.Set.empty
-    var totalRequested: Long = 0L
-    var totalRead: Long = 0L
-
-    var n = 0
-    do {
-      totalRequested += buf.remaining
-      n = ft.read(buf)
-      totalRead += n
-      if (!counts.contains(n)) {
-        counts += n
-        println(counts)
-      }
-      buf.clear()
-    } while (n > 0)
-
-    println(totalRead)
-    ft.close()
-    totalRead
+    val verbose = options contains 'verbose
+    doTransfer(ft, verbose)
   }
 
-  def main(args: Array[String]): Unit = {
-    val api = MythProtocolAPIConnection("myth1")
-    api.announce("Monitor", "dove")
+  sealed trait RecordingSource
+  final case class RecordingSourceId(id: RecordedId) extends RecordingSource
+  final case class RecordingSourceInput(id: CaptureCardId) extends RecordingSource
 
-    for {
-      rec <- api.queryRecorderGetCurrentRecording(CaptureCardId(4))
+  var options: OptionMap = Map.empty
+
+  class RecXferArgParser extends ArgParser {
+    override def accumulateOptions(opts: OptionMap, args: List[String]): OptionMap = args match {
+      case Nil                       => opts
+      case arg :: _ if isOption(arg) => super.accumulateOptions(opts, args)
+      case spec :: cdr               => accumulateOptions(opts ++ recording(spec), cdr)
+      case _                         => super.accumulateOptions(opts, args)
+    }
+
+    def recording(spec: String): OptionMap = Map('recording -> parseRecordingSpec(spec))
+
+    final val InputPattern = """input:(\d+)""".r
+    final val RecIdPattern = """recid:(\d+)""".r
+    final val RecChanTimePattern = """rec:(\d{4})_(\d{14})""".r
+
+    def parseRecordingSpec(spec: String): RecordingSource = spec match {
+      case InputPattern(captureCardId) => RecordingSourceInput(CaptureCardId(captureCardId.toInt))
+      case RecIdPattern(recordedId) => RecordingSourceId(RecordedIdInt(recordedId.toInt))
+      case RecChanTimePattern(chanId, recStartTs) => RecordingSourceId(
+        RecordedIdChanTime(ChanId(chanId.toInt), MythDateTime.fromMythFormat(recStartTs)))
+    }
+  }
+
+  def getRecordingBySource(api: MythProtocolAPI, recSource: RecordingSource): MythProtocolResult[Recording] = {
+    recSource match {
+      case RecordingSourceInput(captureCardId) => api.queryRecorderGetCurrentRecording(captureCardId)
+      case RecordingSourceId(id) => id match {
+        case RecordedIdChanTime(chanId, startTime) => api.queryRecording(chanId, startTime)
+        case recordedIdInt: RecordedIdInt => api match {
+          case _: MythProtocolApi88 =>
+            // There is no API call to query a recording by recordedId, so enumerate all recordings and search.
+            for {
+              recs <- api.queryRecordings()
+              rec = recs.find(r => r.recordedId == recordedIdInt)
+            } yield rec.get
+          case _ => throw new Exception("integer recordedId not supported by this backend protocol version")
+        }
+      }
+    }
+  }
+
+  // TODO progress meter (optional, default)
+
+  override def argParser: ArgParser = new RecXferArgParser
+
+  override def usageArguments: String =
+    s"""<recording>
+      |
+      | where <recording> is one of the following:
+      |     input:<card-input-id>
+      |     recid:<recorded-id>        (for MythTV 0.28+ backends)
+      |     rec:<chanid>_<recstartts>
+      |
+      | examples:
+      |   $usageName input:2
+      |   $usageName recid:1068
+      |   $usageName rec:1081_20180901083000""".stripMargin
+
+  def main(args: Array[String]): Unit = {
+    options = argParser.parseArgs(args)
+
+    val recordingSource = options.get('recording) match {
+      case Some(source: RecordingSource) => source
+      case _ => throw new Exception("no recording source specified")
+    }
+
+    val api = apiMonitorConnection(options)
+    val result = for {
+      rec <- getRecordingBySource(api, recordingSource)
       totalRead = doTransfer(api, rec)
     } yield totalRead
 
-    println("done")
+    import MythProtocolFailure._
+    result match {
+      case Right(totalBytesRead) => println(s"done, $totalBytesRead bytes transferred.")
+      case Left(fail: MythProtocolFailure) => fail match {
+        // TODO improve error messages
+        case MythProtocolNoResult => println("- No result - ")
+        case MythProtocolFailureUnknown => println("- Unknown Failure -")
+        case MythProtocolFailureMessage(message) => println(s"Received error response: $message")
+        case MythProtocolFailureThrowable(throwable) => throwable.printStackTrace()
+      }
+    }
   }
 }
